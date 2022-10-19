0b5a4f1d3e0d29e7d777de32fb543e7396fe0ede
ccbiozhaw <patc@zhaw.ch>
2022-10-19 13:16:59 +0200

    add starship

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 # files
 rm ~/.zshrc
 rm ~/.tmux.conf
+rm ~/.config/starship.toml
 rm -rf ~/.config/alacritty
 
 mkdir -p ~/.config/alacritty
 
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/starship/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/alacritty/* ~/.config/alacritty
 
 
