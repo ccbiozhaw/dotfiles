e6ba2c9086e55dcf727de1b9f93494a2ae250474
ccbiozhaw <patc@zhaw.ch>
2022-10-19 13:13:30 +0200

    dotfiles

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash 
 
 # files
-# rm ~/.zshrc
-# rm ~/.tmux.conf
+rm ~/.zshrc
+rm ~/.tmux.conf
+rm -rf ~/.config/alacritty
+
+mkdir -p ~/.config/alacritty
 
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/alacritty/* ~/.config/alacritty
 
 
 
